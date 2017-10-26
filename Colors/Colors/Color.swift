bbeedb0a837397cfa107127d3bde5431e205044d
Shannon <ldsz89@mail.missouri.edu>
2017-10-26 18:54:24 -0500

    Added Detail View

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Shannon, Leandre D. (MU-Student) on 10/26/17.
+//  Copyright © 2017 Shannon, Leandre D. (MU-Student). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Color {
+    var name: String
+    var uiColor: UIColor
+    
+    init(name: String, uiColor: UIColor) {
+        self.name = name
+        self.uiColor = uiColor
+    }
+}
--- Colors/Colors/ColorViewController.swift
+++ Colors/Colors/ColorViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class ColorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple),
+                  Color(name: "brown", uiColor: UIColor.brown)]
+
     @IBOutlet weak var colorsTableView: UITableView!
     
-    var colors = ["red",
-                  "orange",
-                  "yellow",
-                  "green",
-                  "blue",
-                  "purple",
-                  "brown"]
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Colors"
 
         // Do any additional setup after loading the view.
     }
@@ -41,46 +46,19 @@ class ColorViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
         
-        cell.textLabel?.text = colors[indexPath.row]
-        switch(colors[indexPath.row]) {
-        case "red":
-            cell.backgroundColor = UIColor.red
-        case "orange":
-            cell.backgroundColor = UIColor.orange
-        case "yellow":
-            cell.backgroundColor = UIColor.yellow
-        case "green":
-            cell.backgroundColor = UIColor.green
-        case "blue":
-            cell.backgroundColor = UIColor.blue
-        case "purple":
-            cell.backgroundColor = UIColor.purple
-        case "brown":
-            cell.backgroundColor = UIColor.brown
-        case "black":
-            cell.backgroundColor = UIColor.black
-        case "magenta":
-            cell.backgroundColor = UIColor.magenta
-        case "gray":
-            cell.backgroundColor = UIColor.gray
-        case "cyan":
-            cell.backgroundColor = UIColor.cyan
-        default:
-            cell.backgroundColor = UIColor.white
-            
-        }
+        let color = colors[indexPath.row]
+        
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
         
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? ColorDetailViewController,
+           let row = colorsTableView.indexPathForSelectedRow?.row {
+            destination.color = colors[row]
+        }
     }
-    */
 
 }
